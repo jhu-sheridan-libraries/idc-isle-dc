99ce9c5d935ef8cf78f4f80c225d8107a65ded95
Aaron Birkland <apb@jhu.edu>
2021-07-22 16:11:43 -0400

    Add ability for test suites to disable validation
    Most test suites intentionally migrate invalid objects, due to missing
    required fields that are irrelevant to the test.  This adds a function
    that allows a test suite to specify test-specific environment variables
    (such as DRUPAL_DEFAULT_MIGRATIONS_VALIDATE=false), scoped to that test
    suite

--- tests/.includes.sh
+++ tests/.includes.sh
@@ -23,3 +23,22 @@ function startMigrationAssetsContainer {
   docker run --name ${assets_container} --network gateway --rm -d ${assets_repo}/${assets_image}:${assets_image_tag}
   trap "docker stop ${assets_container}" EXIT
 }
+
+# Re-starts the Docker environment with the given environment variables appended to the existing .env
+# Argument is a string that will be concatenatd with the .env file (i.e. like "FOO=BAR").
+function use_env {
+	if [ -f "docker-compose.yml" ]; then cp docker-compose.yml .docker-compose.yml; fi
+	echo -e "\nUsing additional environment variables and re-loading containers\n\n$1\n\n"
+	ENV_FILE="/tmp/$(date +%s).env"
+	cat .env > "${ENV_FILE}"
+	echo "$1" >> "${ENV_FILE}"
+
+	if [ "$TEST_ENVIRONMENT" == "static" ]; then
+		echo "Using static environment"
+		make -B static-docker-compose.yml env="${ENV_FILE}";
+	else 
+		make -B docker-compose.yml args="--env-file ${ENV_FILE}"
+	fi
+	make up
+	mv .docker-compose.yml docker-compose.yml || make docker-compose.yml
+}
